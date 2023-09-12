d410c8e7970c5771d63576cdff8b13353fee2a62
Philip Yeo <philip.yeo@carlsberggroup.com>
2023-09-12 18:42:23 +0800

    fixed alot of stuff

--- src/main/java/dev/yeopcp/resumeportal/HomeController.java
+++ src/main/java/dev/yeopcp/resumeportal/HomeController.java
@@ -1,13 +1,21 @@
 package dev.yeopcp.resumeportal;
 
+import dev.yeopcp.resumeportal.models.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserProfileRepository userProfileRepository;
 
     @GetMapping("/")
     public String home() {
@@ -21,7 +29,13 @@ public class HomeController {
 
     @GetMapping("/view/{userId}")
     public String view(@PathVariable String userId, Model model) {
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found:" + userId));
+
         model.addAttribute("userId", userId);
-        return "profile";
+        UserProfile userProfile = userProfileOptional.get();
+        model.addAttribute("userProfile", userProfile);
+
+        return "profile-templates/" + userProfile.getId() + "/index";
     }
 }
---
+++ src/main/java/dev/yeopcp/resumeportal/UserProfileRepository.java
@@ -0,0 +1,12 @@
+package dev.yeopcp.resumeportal;
+
+import dev.yeopcp.resumeportal.models.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
+
+    Optional<UserProfile> findByUserName(String userName);
+
+}
